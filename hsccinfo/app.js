aa3b3b51ad755fa487157d1190486a825106556a
blacksword24 <simmonsbb09@gmail.com>
2025-03-22 17:33:28 +0000

    update api functions

--- hsccinfo/app.js
+++ hsccinfo/app.js
@@ -13,7 +13,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var bdpahistoryRouter = require('./routes/bdpahistory');
 var formentryRouter = require('./routes/formentry');
-
+var electionMetaRouter = require('./routes/electionmeta');
 // Setting up Express app
 var app = express();
 
@@ -35,6 +35,7 @@ app.use('/', indexRouter);  //Note that index router connects with /.
 app.use('/users', usersRouter);
 app.use('/bdpahistory', bdpahistoryRouter);
 app.use('/formentry', formentryRouter);
+app.use('/electionmeta', electionMetaRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- hsccinfo/routes/formentry.js
+++ hsccinfo/routes/formentry.js
@@ -2,40 +2,44 @@ var express = require('express');
 var router = express.Router();
 
 const Converter=require("../middleware/NumberConvert");
+
 /* GET formentry page. */
-
 router.get('/', function(req, res, next) {
-
   res.render('formentry', { title: 'Sample Form Entry' });
-
 });
 
 // POST formentry form
 router.post('/', function(req, res, next) {
-let firstname=req.body.firstname;
-let lastname=req.body.lastname;
-let email=req.body.email1;
-let num1=req.body.num1;
-let num2=req.body.num2;
-let usersum=Number(num1)+Number(num2);
+  let firstname=req.body.firstname;
+  let lastname=req.body.lastname;
+  let email=req.body.email1;
+  let num1=req.body.num1;
+  let num2=req.body.num2;
+  let usersum=Number(num1)+Number(num2);
 
-//Display checkbox results
-if (process.env.CONSOLE_DEBUG=="true"){
-console.log(firstname);
-console.log(lastname);
-console.log(num1) // display num1
-console.log(Converter.ConvertDecToBin(num1)) // display binary conversion
-} // End debugging code
+  var binresult1=Converter.ConvertDecToBin(num1)
+  var binresult2=Converter.ConvertDecToBin(num2)
+  //Display checkbox results
+  if (process.env.CONSOLE_DEBUG=="true") {
+    console.log("Convert 1 checkbox",req.body.numconvert1);
+    console.log(req.body.numconvert2);
+    console.log(num1);  // display num1
+    console.log(binresult1) //display binary conversion
+  } // End debugging code
+  console.log("Convert 1 checkbox after debug",req.body.numconvert1);
 
-res.render('formresults', {
-
+  res.render('formresults', {
     title: 'Sample Form Results',
     fn:firstname,
     ln:lastname,
     em:email,
-    total:usersum
+    total:usersum,
+    displaybin:req.body.numconvert1,
+    binnum1:binresult1,
+    binnum2:binresult2
   });
-
 });
+
+
 
 module.exports = router;
